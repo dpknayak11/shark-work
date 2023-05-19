0bdef606dc647b71ef8259fd002d93a7efb88727
Deepak nayak <dpknayak111@gmail.com>
2023-05-19 12:33:50 +0530

    task3: Arrays , spread and rest operators

---
+++ task3.js
@@ -0,0 +1,27 @@
+// Using =  rest,spread, add new elements, using array maps....
+// let arr = ['apple','oranges',' ','mango',' ','lemon'];
+// arr.push('Banana');
+// let allArray = [...arr];
+// var fruits_name = allArray.map( (allArray) => " Fruits Name: " + allArray);
+// console.log(fruits_name);
+
+// ............................................
+
+// const obj1 = {'key1': 1}
+// const obj2 = { ...obj1}
+// if(obj2 === obj1) console.log('same objects')
+// else console.log('different objects');
+// Output:  different objects
+
+// ............................................
+// const obj1 = {'key1': 1 , 'key2' : 2}
+// const obj2 = { ...obj1, key1: 1000}
+// console.log(obj1);
+// console.log(obj2);
+// output: 
+    // { key1: 1, key2: 2 }
+    // { key1: 1000, key2: 2 }
+
+//..............................................
+
+
