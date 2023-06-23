db35c6e3c81287332920c44d81466a01690d7c00
dpknayak11 <107501639+dpknayak11@users.noreply.github.com>
2023-06-23 17:33:05 +0530

    Project: Simple Group Chat Application

--- Task4_chatApp_Project/app.js
+++ Task4_chatApp_Project/app.js
@@ -1,46 +1,69 @@
+// This code is setting up an Express server with a login page and a messages page along with routes for each.
 
-// require packages
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+// require the file system and express libraries
 const fs = require('fs');
+const express = require('express')
 
-// initialize express
+// require body parser to work with post requests
+const bodyParser = require('body-parser')
+
+// create an instance of express
 const app = express();
 
-// require routes (e.g. login)
-const userLogin = require('./routes/login');
-
-// parse url-encoded bodies
+// use body parser to get url params
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// use login route
-app.use(userLogin);
+// create a route for the login page
+app.get('/login', (req, res, next) => {
+    // send an html form to the page which will POST to the /messages route
+    res.send(`<h> 1 .... </h1><form
+    action="/messages" method="post"
+    onsubmit="localStorage.setItem('username', document.getElementById('username').value)">
+    <h> 2 .... </h1>
+    <input type="text" name="username" placeholder="enter username" id="username"/>
+    <h> 3 .... </h1>
+    <button type="submit">Login</button>
+    <h> 4 .... </h1>
+  </form>`)
+});
 
 // GET messages route
 app.get('/messages', (req, res, next) => {
-    fs.readFile('usernames.txt', (err, data) => {
+    // read the data.txt file
+    
+    fs.readFile('data.txt', (err, data) => {
+        // if there is an error, set data to 'No data exists'
         if (err) { data = "No data exists"; }
-        res.send(`<pre> ${data}</pre>
+        // send the file's data inside an html pre tag along with an html form to input messages
+        res.send(`<pre> ${data} </pre>
+        <h> 5 .... </h1>
    <form action="/messages"  onsubmit="document.getElementById('username').value=localStorage.getItem('username' )" method="POST" >
+   <h> 6 .... </h1>
     <input type="text" id="message" name="message" placeholder="msg...">
-    <input type="hidden" name="username" id="username">
+    <h> 7 .... </h1>
+    <input type="text" name="username" id="username">
+    <h> 8 .... </h1>
     <button type="submit">send</button>
+    <h> 9 .... </h1>
     </form>`);
     });
 })
 
 // POST messages route
 app.post('/messages', (req, res, next) => {
+    // console log the incoming message
     console.log(`${req.body.username}: ${req.body.message}`)
-    fs.appendFileSync('usernames.txt', `${req.body.username}: ${req.body.message}\n `);
+    // append the incoming message to data.txt 
+    fs.appendFileSync('data.txt', `${req.body.username}: ${req.body.message}\n `);
+    // redirect to the /messages page
     res.redirect('/messages');
 })
 
 // 404 not found route
-app.use((req, res, next) =>{
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
-});
+app.use((req, res, next) => {
+    // send a 404 message if page is not found
+    res.status(404).send(`<h1>page note found!</h1>`)
+})
 
-// listen for incoming requests
-app.listen(3000);
+// start listening to incoming requests on port 5000
+app.listen(5000);
