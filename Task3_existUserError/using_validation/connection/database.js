78922f4aecc59b91dd55cc70125635cbd9066995
Deepak Nayak <107501639+dpknayak11@users.noreply.github.com>
2023-08-11 17:42:53 +0530

    Delete Task3_existUserError directory

--- Task3_existUserError/using_validation/connection/database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize').Sequelize;
-
-const sequelize = new Sequelize('expense_project', 'root', 'root', {
-    dialect:'mysql', host: 'localhost'
-})
-
-module.exports = sequelize;
