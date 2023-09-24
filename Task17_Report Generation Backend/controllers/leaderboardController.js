1a6b602076173b75e5c8a33ba4f7f8c0c0eca025
dpknayak11 <dpknayak111@gmail.com>
2023-09-24 17:45:34 +0530

    Report Generation Backend (Premium Feature)

---
+++ Task17_Report Generation Backend/controllers/leaderboardController.js
@@ -0,0 +1,6 @@
+const path = require("path");
+exports.getLeaderboardPage = async (req, res, next) => {
+  try {
+    res.sendFile(path.join(__dirname, "../", "views", "leaderboard-page.html"));
+  } catch { (err) => console.log(err); }
+};
